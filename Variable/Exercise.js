73616b87e87616b44b6e36d5b6b3990e698d3b51
KAROL-OKOEDE <karol.okoede@gmail.com>
2023-04-15 08:58:26 +0100

    madlibs game using variables in javascript

---
+++ Variable/Exercise.js
@@ -0,0 +1,11 @@
+// Create madlibs game using variables in java
+let pluralNoun = "Violets";
+let pluralNoun2 = "Orchids";
+let singularNoun = "you";
+let singularNoun2 = "loved";
+let color = "purple";
+let color2 = "white";
+
+console.log (pluralNoun  +  "are" +  color)
+console.log (pluralNoun2  +  "are" +  color2)
+console.log (singularNoun  +  "are" +  singularNoun2)
